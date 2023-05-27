30ae1d5a287e30f53ef39eca30547f2abeb2bdc0
ali139713 <aliabdullahazhar@gmail.com>
2023-05-27 23:38:44 +0500

    Add background gradient.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 
-import Image from "next/image";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
-import Avatar from 'react-avatar';
+import Image from "next/image";
+import Avatar from "react-avatar";
 
 function Header() {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div className="absolute top-0 left-0 h-96 w-full bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50"></div>
+
         <Image
           src="https://links.papareact.com/c2cdd5"
           alt="Trello Logo"
@@ -28,13 +30,12 @@ function Header() {
             </button>
           </form>
 
-    <Avatar name="Ali Abdullah" round size="50" color="#009283" />
-
+          <Avatar name="Ali Abdullah" round size="50" color="#009283" />
         </div>
       </div>
-      <div className="flex items-center justify-center px-5 md:py-5">
+      <div className="flex items-center justify-center px-5 py-2 md:py-5">
         <p className="flex items-center w-fit p-5 text-sm font-light text-[#009283] italic shadow-xl rounded-xl bg-white max-w-3xl">
-          <UserCircleIcon className="inline-block h-10 w-10 text-[#009283] mr-1"/>  
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#009283] mr-1" />
           AI is condensing your daily tasks into a summary.
         </p>
       </div>
