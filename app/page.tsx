91cfd240b6b0005cd23e6d99034bc573d1868d5e
ali139713 <aliabdullahazhar@gmail.com>
2023-05-27 15:41:23 +0500

    Remove boiler plate code.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Header from '@/components/Header'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>OpenAI Powered Trelllo 2.0</h1>
+     <Header />
     </main>
   )
 }
