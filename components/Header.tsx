5417515da369a9872ea92dfacca97e95cb288a35
ali139713 <aliabdullahazhar@gmail.com>
2023-05-27 17:15:07 +0500

    Styled AI suggestion text.

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
           height={100}
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
         />
-        <div className="">
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
             <input
@@ -31,6 +31,12 @@ function Header() {
     <Avatar name="Ali Abdullah" round size="50" color="#009283" />
 
         </div>
+      </div>
+      <div className="flex items-center justify-center px-5 md:py-5">
+        <p className="flex items-center w-fit p-5 text-sm font-light text-[#009283] italic shadow-xl rounded-xl bg-white max-w-3xl">
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#009283] mr-1"/>  
+          AI is condensing your daily tasks into a summary.
+        </p>
       </div>
     </header>
   );
