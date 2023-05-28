4acd143416cd0fc31b6394bb9996e0e56707fee2
ali139713 <aliabdullahazhar@gmail.com>
2023-05-28 18:37:55 +0500

    Fix draggable columns not rendering.

--- components/Column.tsx
+++ components/Column.tsx
@@ -6,7 +6,18 @@ type Props = {
   index: number;
 };
 
+const columnMapping: {
+  [key in TypedColumn]: string;
+} = {
+  Todo: "Todo",
+  InProgress: "In Progress",
+  Done: "Done",
+};
+
 function Column({ id, todos, index }: Props) {
+
+  console.log('todos in column :',todos)
+
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -16,15 +27,20 @@ function Column({ id, todos, index }: Props) {
           ref={provided.innerRef}
         >
           <Droppable droppableId={index.toString()} type="card">
-            {(provided, snapshot):any => {
+            {(provided, snapshot) => (
               <div
                 {...provided.droppableProps}
                 ref={provided.innerRef}
                 className={`p-2 rounded-2xl shadow-sm ${
                   snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
                 }`}
-              ></div>;
-            }}
+              >
+                <h2 className="flex justify-between font-bold text-xl p-2">
+                    {columnMapping[id]}
+                <span className="text-gray bg-gray-200 rounded-full px-2 py-2 text-sm">{todos.length}</span>
+                </h2>
+              </div>
+            )}
           </Droppable>
         </div>
       )}
