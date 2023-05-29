d2b7c44f09a86aaab714de23fb2352d3c18e6a15
ali139713 <aliabdullahazhar@gmail.com>
2023-05-29 17:17:33 +0500

    Complet column drag functionality.

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,10 +6,11 @@ import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import Column from "./Column";
 
 function Board() {
-  const [board, getBoard, setBoardState] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore((state) => [
     state.board,
     state.getBoard,
-    state.setBoardState
+    state.setBoardState,
+    state.updateTodoInDB
   ]);
 
   useEffect(() => {
@@ -36,7 +37,64 @@ function Board() {
 
     // Handle card drag
 
-    
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: IColumn = {
+        id: startColIndex[0],
+        todos: startColIndex[1].todos,
+    };
+
+    const finishCol: IColumn = {
+        id: finishColIndex[0],
+        todos: finishColIndex[1].todos,
+    };
+
+    if(!startCol || !finishCol) return;
+
+    if(source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if(startCol.id === finishCol.id){
+        // same column task drag
+
+        newTodos.splice(destination.index, 0, todoMoved);
+        const newCol: IColumn = {
+            id: startCol.id,
+            todos: newTodos
+        }
+
+        const newColumns = new Map(board.columns);
+        newColumns.set(startCol.id, newCol);
+
+        setBoardState({...board, columns: newColumns})
+    }
+    else {
+        // dragging to another column
+        const finishTodos = Array.from(finishCol.todos);
+        finishTodos.splice(destination.index, 0 , todoMoved);
+
+        const newColumns = new Map(board.columns);
+        const newCol = {
+            id: startCol.id,
+            todos: newTodos,
+        };
+
+        newColumns.set(startCol.id, newCol);
+        newColumns.set(finishCol.id, {
+            id:finishCol.id,
+            todos: finishTodos,
+        });
+
+        // Update in DB
+        updateTodoInDB(todoMoved, finishCol.id)
+
+        setBoardState({...board, columns: newColumns});
+    }
+
 
   };
 
