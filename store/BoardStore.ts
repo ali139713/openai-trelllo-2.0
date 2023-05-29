d968a9ef3ab3cbee59da6e51f04ad2d5c33b8dc4
ali139713 <aliabdullahazhar@gmail.com>
2023-05-29 17:17:42 +0500

    Create method to update todo in db.

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,22 +1,34 @@
-import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
-import { create } from 'zustand'
-import { devtools, persist } from 'zustand/middleware'
+import { databases } from "@/appwrite";
+import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn";
+import { create } from "zustand";
 
 interface BoardState {
   board: IBoard;
   getBoard: () => void;
-  setBoardState: (board:IBoard) => void;
+  setBoardState: (board: IBoard) => void;
+  updateTodoInDB: (todo: ITodo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
-    board:{
-        columns: new Map<TypedColumn, IColumn>()
-    },
-    getBoard: async () => {
-        const board = await getTodosGroupedByColumn();
-        set({board});
-    },
+  board: {
+    columns: new Map<TypedColumn, IColumn>(),
+  },
+  getBoard: async () => {
+    const board = await getTodosGroupedByColumn();
+    set({ board });
+  },
 
-    setBoardState: (board) => set({board})
-}))
+  setBoardState: (board) => set({ board }),
 
+  updateTodoInDB: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
+  },
+}));
