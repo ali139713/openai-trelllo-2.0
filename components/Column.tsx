a2c6a125d220d08bf6af2aac2f314224193a80e0
ali139713 <aliabdullahazhar@gmail.com>
2023-05-29 16:25:19 +0500

    Create UI for adding new todo.

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,3 +1,4 @@
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 
 type Props = {
@@ -15,8 +16,7 @@ const columnMapping: {
 };
 
 function Column({ id, todos, index }: Props) {
-
-  console.log('todos in column :',todos)
+  console.log("todos in column :", todos);
 
   return (
     <Draggable draggableId={id} index={index}>
@@ -36,9 +36,39 @@ function Column({ id, todos, index }: Props) {
                 }`}
               >
                 <h2 className="flex justify-between font-bold text-xl p-2">
-                    {columnMapping[id]}
-                <span className="text-gray bg-gray-200 rounded-full px-2 py-2 text-sm">{todos.length}</span>
+                  {columnMapping[id]}
+                  <span className="text-gray bg-gray-200 rounded-full px-2 py-2 text-sm">
+                    {todos.length}
+                  </span>
                 </h2>
+                <div className="space-y-2">
+                  {todos.map((todo, index) => (
+                    <Draggable
+                      key={todo.$id}
+                      draggableId={todo.$id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <TodoCard
+                          todo={todo}
+                          index={index}
+                          id={id}
+                          innerRef={provided.innerRef}
+                          draggableProps={provided.draggableProps}
+                          dragHandleProps={provided.dragHandleProps}
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+
+                  {provided.placeholder}
+
+                  <div className="flex items-end justify-end p-2">
+                    <button className="text-green-500 hover:text-green-600">
+                        <PlusCircleIcon className="h-10 w-10"/>
+                    </button>
+                  </div>
+                </div>
               </div>
             )}
           </Droppable>
