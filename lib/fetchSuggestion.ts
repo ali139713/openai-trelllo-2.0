7492820bb3ea863ae8673fc54bb12d68001bfd5c
ali139713 <aliabdullahazhar@gmail.com>
2023-05-30 12:47:00 +0500

    Create helper function for calling generate summary API.

---
+++ lib/fetchSuggestion.ts
@@ -0,0 +1,18 @@
+const fetchSuggestion = async (board: IBoard) => {
+  const todos = formatTodoForGPT(board);
+
+  const res = await fetch("/api/generateSummary", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ todos }),
+  });
+
+  const gptData = await res.json();
+  const { content } = gptData;
+
+  return content;
+};
+
+export default fetchSuggestion;
