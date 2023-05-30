36f56199c32b06f594b4329f6c26a80dfed7e79f
ali139713 <aliabdullahazhar@gmail.com>
2023-05-30 12:44:48 +0500

    Create helper function for formatting data for GPT.

---
+++ lib/formatTodoForGPT.ts
@@ -0,0 +1,20 @@
+const formatTodoForGPT = (board: IBoard) => {
+  const todos = Array.from(board.columns.entries());
+
+  const flatArray = todos.reduce((map, [key, value]) => {
+    map[key] = value.todos;
+    return map;
+  }, {} as { [key in TypedColumn]: ITodo[] });
+
+  // format to key: value(length)
+
+  const flatArrayCounted = Object.entries(flatArray).reduce(
+    (map, [key, value]) => {
+      map[key as TypedColumn] = value.length;
+      return map;
+    },
+    {} as { [key in TypedColumn]: number }
+  );
+
+  return flatArrayCounted;
+};
